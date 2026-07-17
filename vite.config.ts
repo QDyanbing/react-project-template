af0fe2f5bcd7573bc85b1e5b2bb095ade3532a94
高艳兵 <yanbing.gao@aliyun.com>
2026-07-17 15:07:05 +0800

    chore: configure module path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import babel from '@rolldown/plugin-babel';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@config': fileURLToPath(new URL('./config', import.meta.url)),
+    },
+  },
   server: {
     port: 8000,
   },

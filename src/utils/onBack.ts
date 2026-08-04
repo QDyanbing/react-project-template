78ebc8e017d5317f2604aa3fc17f3ecdd8c467eb
高艳兵 <yanbing.gao@aliyun.com>
2026-08-04 14:00:36 +0800

    refactor: remove legacy back utility

--- src/utils/onBack.ts
+++
@@ -1,9 +0,0 @@
-import { router } from '@config/router';
-
-export default (defaultPath: string) => {
-  if (router.history.canGoBack()) {
-    router.history.back();
-  } else {
-    router.navigate({ to: defaultPath, replace: true });
-  }
-};

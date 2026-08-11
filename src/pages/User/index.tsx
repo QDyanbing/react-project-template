33884fb4d81c0bfe65dc0d7fd31147e96d819722
高艳兵 <yanbing.gao@aliyun.com>
2026-08-11 11:33:05 +0800

    feat(user): add user list page

---
+++ src/pages/User/index.tsx
@@ -0,0 +1,157 @@
+import { Button, Pagination, Popconfirm, Space, Spin, Table, Tag, Typography } from 'antd';
+import { useEffect } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+
+import Permission from '@/components/Permission';
+import { formatTime } from '@/utils/format';
+import { onHistoryChange } from '@/utils/history';
+import SearchBar from './components/SearchBar';
+import useDelete from './hooks/useDelete';
+import useDisable from './hooks/useDisable';
+import useEnable from './hooks/useEnable';
+import useResetPassword from './hooks/useResetPassword';
+import styles from './index.module.less';
+import useData from './models/useData';
+import usePage from './models/usePage';
+
+export default () => {
+  const { mount, unmount, params, onPaginationChange } = usePage();
+  const { loading, data, total } = useData();
+
+  const { loading: deleteLoading, onDelete } = useDelete();
+  const { loading: disableLoading, onDisable } = useDisable();
+  const { loading: enableLoading, onEnable } = useEnable();
+  const { loading: passwordLoading, onResetPassword } = useResetPassword();
+
+  const { t } = useTranslation('user');
+
+  const columns = [
+    { title: t('column.account'), dataIndex: 'account' },
+    { title: t('column.name'), dataIndex: 'name' },
+    {
+      title: t('column.roles'),
+      dataIndex: 'roles',
+      render: (roles: API.Role[]) => (
+        <Space wrap size="small">
+          {roles.map((role) => (
+            <Tag key={role.uuid}>{role.name}</Tag>
+          ))}
+        </Space>
+      ),
+    },
+    {
+      width: 100,
+      title: t('column.status'),
+      dataIndex: 'status',
+      render: (status: API.User['status']) => (
+        <Tag color={status === 'enabled' ? 'success' : 'default'}>{t(`status.${status}`)}</Tag>
+      ),
+    },
+    {
+      width: 180,
+      title: t('column.gmtCreate'),
+      dataIndex: 'gmtCreate',
+      render: formatTime,
+    },
+    {
+      width: 380,
+      title: t('column.action'),
+      dataIndex: 'uuid',
+      render: (uuid: string, user: API.User) => (
+        <Space size="small">
+          <Button
+            type="link"
+            className={styles.action}
+            onClick={() => onHistoryChange('/users/detail', { uuid })}
+          >
+            {t('action.detail')}
+          </Button>
+          <Permission permissions="user:modify">
+            <Button
+              type="link"
+              className={styles.action}
+              onClick={() => onHistoryChange('/users/modify', { uuid })}
+            >
+              {t('action.modify')}
+            </Button>
+          </Permission>
+          {user.status === 'enabled' ? (
+            <Popconfirm title={t('action.disableConfirm')} onConfirm={() => onDisable(uuid)}>
+              <Permission permissions="user:disable">
+                <Button type="link" className={styles.action}>
+                  {t('action.disable')}
+                </Button>
+              </Permission>
+            </Popconfirm>
+          ) : (
+            <Popconfirm title={t('action.enableConfirm')} onConfirm={() => onEnable(uuid)}>
+              <Permission permissions="user:enable">
+                <Button type="link" className={styles.action}>
+                  {t('action.enable')}
+                </Button>
+              </Permission>
+            </Popconfirm>
+          )}
+          <Popconfirm
+            title={t('action.resetPasswordConfirm')}
+            onConfirm={() => onResetPassword(uuid)}
+          >
+            <Permission permissions="user:reset-password">
+              <Button type="link" className={styles.action}>
+                {t('action.resetPassword')}
+              </Button>
+            </Permission>
+          </Popconfirm>
+          <Popconfirm title={t('action.deleteConfirm')} onConfirm={() => onDelete(uuid)}>
+            <Permission permissions="user:delete">
+              <Button danger type="link" className={styles.action}>
+                {t('action.delete')}
+              </Button>
+            </Permission>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
+  useEffect(() => {
+    mount();
+
+    return unmount;
+  }, [mount, unmount]);
+
+  return (
+    <Spin
+      spinning={loading || deleteLoading || disableLoading || enableLoading || passwordLoading}
+      classNames={{ root: styles.user, container: styles.container }}
+    >
+      <SearchBar />
+      <Table
+        sticky
+        pagination={false}
+        rowKey="uuid"
+        columns={columns}
+        dataSource={data}
+        className={styles.body}
+      />
+      <Pagination
+        showSizeChanger
+        align="end"
+        current={params.pageNum}
+        disabled={loading}
+        pageSize={params.pageSize}
+        total={total}
+        className={styles.footer}
+        showTotal={(value) => (
+          <Trans
+            i18nKey="pagination.total"
+            t={t}
+            values={{ count: value }}
+            components={{ count: <Typography.Text strong type="success" /> }}
+          />
+        )}
+        onChange={onPaginationChange}
+      />
+    </Spin>
+  );
+};

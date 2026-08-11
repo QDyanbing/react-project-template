333943ced354e8b36c40b76bfed8f97b769eadd9
高艳兵 <yanbing.gao@aliyun.com>
2026-08-11 10:29:06 +0800

    feat(user): add password reset action

---
+++ src/pages/User/hooks/useResetPassword.ts
@@ -0,0 +1,22 @@
+import { useRequest } from 'ahooks';
+import { App } from 'antd';
+import { useTranslation } from 'react-i18next';
+
+import { setResetPassword } from '@/services/user';
+
+export default () => {
+  const { t } = useTranslation('user');
+
+  const { message } = App.useApp();
+
+  const { loading, runAsync } = useRequest(setResetPassword, { manual: true });
+
+  const onResetPassword = async (uuid: string) => {
+    const result = await runAsync(uuid);
+    if (!result) return;
+
+    message.success(t('message.resetPasswordSuccess', { password: result.data.password }));
+  };
+
+  return { loading, onResetPassword };
+};

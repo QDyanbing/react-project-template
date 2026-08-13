e97cd4a734a211409658aba90d41e03e23d85c9b
高艳兵 <yanbing.gao@aliyun.com>
2026-08-13 13:32:47 +0800

    feat: add role detail translations

---
+++ src/pages/RoleDetail/locale/en-US.ts
@@ -0,0 +1,13 @@
+export default {
+  language: 'en-US',
+  namespace: 'roleDetail',
+  resources: {
+    name: 'Role name',
+    description: 'Description',
+    permissions: 'Permissions',
+    users: 'Assigned users',
+    gmtCreate: 'Created at',
+    back: 'Back',
+    modify: 'Edit role',
+  },
+} satisfies I18n.Locale;
---
+++ src/pages/RoleDetail/locale/zh-CN.ts
@@ -0,0 +1,13 @@
+export default {
+  language: 'zh-CN',
+  namespace: 'roleDetail',
+  resources: {
+    name: '角色名称',
+    description: '角色描述',
+    permissions: '权限标识',
+    users: '关联用户数',
+    gmtCreate: '创建时间',
+    back: '返回',
+    modify: '编辑角色',
+  },
+} satisfies I18n.Locale;

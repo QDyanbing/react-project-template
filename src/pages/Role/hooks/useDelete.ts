d2b1abe56adb98032e8c0ad6574f0e357290f050
高艳兵 <yanbing.gao@aliyun.com>
2026-08-13 10:05:33 +0800

    feat(role): add delete action

---
+++ src/pages/Role/hooks/useDelete.ts
@@ -0,0 +1,25 @@
+import { setDelete } from '@/services/role';
+import { useRequest } from 'ahooks';
+import { App } from 'antd';
+import { useTranslation } from 'react-i18next';
+import useData from '../models/useData';
+
+export default () => {
+  const { onRefresh } = useData.getState();
+
+  const { t } = useTranslation('role');
+
+  const { message } = App.useApp();
+
+  const { loading, runAsync } = useRequest(setDelete, { manual: true });
+
+  const onDelete = async (uuid: string) => {
+    const result = await runAsync(uuid);
+    if (!result) return;
+
+    message.success(t('message.deleteSuccess'));
+    onRefresh();
+  };
+
+  return { loading, onDelete };
+};
